e73e5c01377efaacadec78802ece38c6735b4d7e
JhonnBo <67331409+JhonnBo@users.noreply.github.com>
2023-03-31 21:00:32 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Extension_Methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = "Hello World".CharCount('o');
+            Console.WriteLine("Hello, World! contain o: " + count + " times");
+
+            count = "Hello World".CharCount('l');
+            Console.WriteLine("Hello, World! contain l: " + count + " times");
+
+            MyMath myMath = new MyMath();
+            myMath.Square(5);
+            Console.WriteLine("Проверка на четность: " + myMath.MyMathEven(5));
+
+        }
+    }
+
+    static class Extentions
+    {
+        public static int  CharCount(this string str, char c)
+        {
+            int counter = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] == c)
+                    counter++;
+            }
+            return counter;
+        }
+        public static bool MyMathEven(this MyMath myMath, int x) => x % 2 == 0;
+        
+    }
+
+    sealed class MyMath
+    {
+        public void Square(int x)
+        {
+            Console.WriteLine($"Square {x} is {x * x}");
+        }
+    }
+    // class MyClass : MyMath { } // Нельзя! Запрещено!
+}
